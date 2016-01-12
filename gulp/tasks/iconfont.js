e24c8346b898da3bdafcad2fa3299b177f9d10eb
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2016-01-12 18:39:00 +0200

    init

--- gulp/tasks/iconfont.js
+++ gulp/tasks/iconfont.js
@@ -34,8 +34,8 @@ gulp.task('font', function(){
                 glyphs: glyphs,
                 fontName: fontname,
                 fontPath: 'fonts/',
-                className: 'icon',
-                htmlBefore: '<i class="icon ',
+                className: 'i-icon',
+                htmlBefore: '<i class="i-icon ',
                 htmlAfter: '"></i>',
                 htmlBr: ''
             }))
