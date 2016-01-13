13ee81236a54803ca893608fff9c7f6e9bcf591e
etna-scorpio <etna.scorpio@gmail.com>
2016-01-13 19:31:52 +0200

    sidebar started

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
-	// go js
+	// sidebar
+	$('.js-sublink').click(function(e) {
+		e.preventDefault();
+		if ( !$(this).hasClass('is-expanded') ) {
+			$(this).addClass('is-expanded');
+			$(this).parent().siblings().find('.js-sublink').removeClass('is-expanded');
+			$(this).parent().siblings().find('.js-submenu').slideUp();
+			$(this).parent().find('.js-submenu').slideDown();
+		}
+
+	});
 
 });
